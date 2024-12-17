04eafba41f8f04dd3575e46bf5b036e463b8f962
Fai Alateeq <fairouzalatteq@gmail.com>
2024-12-17 09:09:37 -0600

    fixed shoppingCartController paths

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -58,7 +58,7 @@ public class ShoppingCartController
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
-    @PostMapping("products/{id}")
+    @PostMapping("products/{productId}")
 
     public ShoppingCart createShoppingCart(@RequestBody ShoppingCart shoppingCart){
         return shoppingCartDao.create(shoppingCart);
